f23831a1fa48cb408734420e2ab4647d5103b6cf
Chase Bonin <czbonin@yahoo.com>
2025-01-12 11:09:09 -0500

    Updated all instances of scikit-learn to 1.5.2 in conda.yml files.

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -92,11 +92,9 @@ def go(args):
 
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
     # HINT: use mlflow.sklearn.save_model
-    signature = mlflow.models.infer_signature(X_val, y_pred)
     mlflow.sklearn.save_model(
         sk_model=sk_pipe,
         path="random_forest_dir",
-        signature = signature,
         input_example = X_train.iloc[:5]
     )
 
